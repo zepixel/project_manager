060bcb93beac87b6c797d1d143a5780b1259ade1
Pixel <pixel@htkc.org>
2018-02-15 01:52:29 +0100

    maj

--- __main__.py
+++ __main__.py
@@ -5,9 +5,15 @@ import xlsconverter
 from projectreader import clear, session
 
 
-# hello world,
-clear()
-print("\nCe script vous permet de charger des projets etudiants, de les noter et les commenter, puis de les retranscrire dans le fichier excel de section et de les mettre en ligne sous forme d'une page web.\n")
-session_0 = session("NOM","./session/session.json")
-session_0.selection()
-session_0.run()
+while True:
+    try:
+        # hello world,
+        clear()
+        print("\nCe script vous permet de charger des projets etudiants, de les noter et les commenter, puis de les retranscrire dans le fichier excel de section et de les mettre en ligne sous forme d'une page web.\n")
+        session_0 = session("NOM","./session/session.json")
+        session_0.selection()
+        session_0.run()
+        break
+    
+    except (KeyboardInterrupt):
+        break
